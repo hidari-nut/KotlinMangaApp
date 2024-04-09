869371de8c789b59217ab4020aa622f7d2c85c8b
hidari-nut <madevis02@gmail.com>
2024-04-09 18:19:54 +0700

    Initial commit

---
+++ app/src/main/java/com/viswa/hobbyapp_160421069/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.viswa.hobbyapp_160421069.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.viswa.hobbyapp_160421069.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
